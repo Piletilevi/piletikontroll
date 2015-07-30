93363a777e3967d8da46892438e2a2b96f314cc3
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-07-31 00:58:17 +0300

    test

--- javascripts/main.js
+++ javascripts/main.js
@@ -41,7 +41,7 @@ $.ajax({'url': configuration.ENTU_API_TERMINALS })
 .done(function Ok( data ) {
     result = data.result
     result.forEach(function terminalLoop(t) {
-        console.log(t)
+        // console.log(t)
         var new_terminal = $('<option>').text(t.name)
         $('#select_terminal').append(new_terminal)
         $.ajax({'url': configuration.ENTU_API_ENTITY + '-' + t.id })
@@ -58,35 +58,55 @@ $.ajax({'url': configuration.ENTU_API_TERMINALS })
     console.log(data)
 })
 
+var q_options = {op: 'T', ticket: '', ticket_id: ''}
+
 $('#select_terminal').change(function(event) {
     $('#select_terminal_row').addClass('hidden')
     $('#terminal_name').text($('option[value="' + $('#select_terminal').val() + '"]').text())
     $('#terminal_name_row').removeClass('hidden')
     $('#ticket_check_row').removeClass('hidden')
+    q_options['term'] = $('#select_terminal').val()
     console.log($('#select_terminal').val())
     console.log($('option[value="' + $('#select_terminal').val() + '"]').text())
 })
 
-var ticket_q = ''
+var ticket_par = ''
+var ticket_val = ''
 $('#barcode_input').change(function(event) {
     $('#ticket_id_input').val('')
-    ticket_q = 'ticket=' + $('#barcode_input').val()
+    q_options['ticket'] = $('#barcode_input').val()
+    delete q_options['ticket_id']
 })
 $('#ticket_id_input').change(function(event) {
     $('#barcode_input').val('')
-    ticket_q = 'ticket_id=' + $('#ticket_id_input').val()
+    q_options['ticket_id'] = $('#ticket_id_input').val()
+    delete q_options['ticket']
 })
 $('#submit_ticket').click(function(event) {
-    console.log(configuration.TICKETSTATUS_URL + '?term=' + $('#select_terminal').val() + '&op=T&' + ticket_q)
-        $.ajax({'url': configuration.TICKETSTATUS_URL + '?term=' + $('#select_terminal').val() + '&op=T&' + ticket_q })
-// ?term=710227&op=T&ticket=43200708698504'
-        .done(function Ok( data ) {
-            console.log(data)
-            $('#result_row').text(data)
+    // console.log(configuration.TICKETSTATUS_URL + '?term=' + $('#select_terminal').val() + '&op=T&' + ticket_q)
+    $.getJSON(configuration.TICKETSTATUS_URL, q_options, function(json, textStatus) {
+            /*optional stuff to do after success */
+        console.log(json)
+        $('#result_rows').text('')
+        json.rows.forEach(function rowLoop(r) {
+            console.log(r)
+            var row_element = $('<div>')
+            row_element.addClass('status_' + json.status)
+            row_element.text(r)
+            $('#result_rows').append(row_element)
         })
-        .fail(function Fail( data ) {
-            console.log(data)
-        })
+            // $('#result_rows').text(json.status)
+    })
+
+//         $.ajax({'url': configuration.TICKETSTATUS_URL + '?term=' + $('#select_terminal').val() + '&op=T&' + ticket_q })
+// // ?term=710227&op=T&ticket=43200708698504'
+//         .done(function Ok( data ) {
+//             console.log(data)
+//             $('#result_row').text(data)
+//         })
+//         .fail(function Fail( data ) {
+//             console.log(data)
+//         })
 
     return false
 })
