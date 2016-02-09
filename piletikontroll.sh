a8a6563476a26801918bae37b847d876260ab47a
Argo Roots <argo@roots.ee>
2016-02-09 10:10:19 +0200

    piletikontroll.sh

--- piletikontroll.sh
+++ piletikontroll.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-mkdir -p /data/piletikontroll/code
+mkdir -p /data/piletikontroll/code /data/piletikontroll/log
 cd /data/piletikontroll/code
 
 git clone -q https://github.com/Piletilevi/piletikontroll.git ./
 git checkout -q master
 git pull
-printf "\n\n"
 
+printf "\n\n"
 version=`date +"%y%m%d.%H%M%S"`
-docker build -q -t piletikontroll:$version ./ && docker tag -f piletikontroll:$version piletikontroll:latest
-printf "\n\n"
+docker build --quiet --pull --tag=piletikontroll:$version ./ && docker tag piletikontroll:$version piletikontroll:latest
 
+printf "\n\n"
 docker stop piletikontroll
 docker rm piletikontroll
 docker run -d \
+    --net="entu" \
     --name="piletikontroll" \
     --restart="always" \
+    --cpu-shares=256 \
     --memory="512m" \
+    --env="NODE_ENV=production" \
+    --env="VERSION=$version" \
     --env="PORT=80" \
     --env="PL_URL=" \
     --env="NEW_RELIC_APP_NAME=piletikontroll" \
@@ -28,7 +32,5 @@ docker run -d \
     --env="SENTRY_DSN=" \
     piletikontroll:latest
 
-docker inspect -f "{{ .NetworkSettings.IPAddress }}" piletikontroll
 printf "\n\n"
-
-/data/nginx.sh
+docker exec nginx /etc/init.d/nginx reload
